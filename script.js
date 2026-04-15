f49760d31b7f337acf1922a238cff582cca1aa59
Paasilvac <paasilvac>
2026-04-15 00:16:03 -0300

    Projeto Livro autobiografico

---
+++ script.js
@@ -0,0 +1,16 @@
+const btnTopo = document.getElementById("btnTopo");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    btnTopo.style.display = "block";
+  } else {
+    btnTopo.style.display = "none";
+  }
+});
+
+btnTopo.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
